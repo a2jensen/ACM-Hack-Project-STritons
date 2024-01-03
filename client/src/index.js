aa948ab47267b7d8d137c6ed0c68a30721bfab2f
a2jensen <a2jensen@ucsd.edu>
2024-01-03 13:57:59 +0800

    Added comments to index.js file

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// entry point for a react application
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
